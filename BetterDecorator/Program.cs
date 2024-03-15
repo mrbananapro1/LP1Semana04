320db29f804e5abb28a035e4ba2ac7eb9413f980
Leonardo Henrique <leonardojose@netcabo.pt>
2024-03-15 14:29:58 +0000

    If args statement

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
