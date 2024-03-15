157dc6c1b20ca103b88c63f9339c8df6376feb2e
Leonardo Henrique <leonardojose@netcabo.pt>
2024-03-15 14:24:34 +0000

    If statement

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,25 @@ namespace Special
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetSpecial(0)); // Devolve 0
+            Console.WriteLine(GetSpecial(2)); // Devolve 1
+            Console.WriteLine(GetSpecial(7)); // Devolve 43
+        }
+        private static int GetSpecial(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                // Aplicando a fórmula para os números seguintes na sequência
+                return 2 * GetSpecial(n - 1) + GetSpecial(n - 2);
+            }
         }
     }
 }
